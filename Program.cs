bc70b9710f2fde5fba926d4c599cdda6d666fca4
Juan Guevara <65415598+gueca@users.noreply.github.com>
2023-03-29 01:16:58 -0300

    Hola mundo en c#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido");
+            Console.Write("Hola, mi nombre es Juan Guevara y este es mi primer acercamiento con c#"); // Solo sirve para los textos de una sola linea
+            Console.ReadKey(); //Esto sirve cuando se ejecuta el programa, el usuario debe presionar una tecla para finalizar.   
+
+        }
+    }
+}
